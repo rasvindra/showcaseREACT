1c940149723ab33c8c081885c59c2bd3437f7f08
Ravi Nagi <rasvindra@hotmail.com>
2023-03-08 14:41:26 -0800

    still trying to fix component errors

--- src/components/ContactMEForm.js
+++ src/components/ContactMEForm.js
@@ -7,7 +7,7 @@ function ContactMEForm() {
         <Form>
           <Form.Group className="mb-3" id="Contact">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Please conatct Me" />
+            <Form.Control type="email" placeholder="Please contact Me" />
             <Form.Text className="text-muted">
               I look forward to hearing from you. Thank you!.
             </Form.Text>
