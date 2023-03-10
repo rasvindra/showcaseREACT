2bee64ce30ee02e69b123c8e38e94eb022c91adf
Ravi Nagi <rasvindra@hotmail.com>
2023-03-09 17:26:04 -0800

    working on rendering parts of the card for each project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import PinkClouds from "../Assets/pink-clouds.jpg" 
 
 
 function ProjectCards(props) {
+
     return (
         <Card className="ProjectCardTemplate">
             
-            <Card.Body variant="top">
-                <Card.Img variant="bottom" src={props.imgPath} alt="short description"/>
-                <Card.Title variant="top">{props.title}</Card.Title>
-                <Card.Text style={{textAlign: "justify"}}>{props.description}</Card.Text>
+            <Card.Body style={{background:`url(${PinkClouds})`, backgroundSize:"cover", backgroundRepeat:"no-repeat", padding:"25px"}} variant="top">
+                {/* <Card.Img variant="bottom" src={props.imgPath} alt="short description"/> */}
+                <Card.Title style={{size:"40px"}} variant="top">{props.title}</Card.Title>
+                <Card.Text style={{textAlign: "justify", padding:"5px"}}>{props.description}</Card.Text>
                 <Button variant="primary" href={props.GitHubLink} target="_blank"></Button>
-                <Button variant="secondary" href={props.DeployedLink} target="_blank"></Button>
+                <Button variant="primary" href={props.DeployedLink} target="_blank"></Button>
             </Card.Body>
         </Card>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,11 +9,11 @@ function Projects() {
     return(
         <Container fluid className="project-area" id="Portfiolio">
             <Container>
-                <h1 style={{justifyContent:"center"}}> Here is a list of some of my Projects and Collaberations</h1>
-                <Row style={{justifyContent:"center"}}>
+                <h1 style={{justify:"center"}}> Here is a list of some of my Projects and Collaberations</h1>
+                <Row>
                     <Col md={4} className="project-card">
                         <ProjectCards
-                        imgPath={PinkClouds}
+                        // imgPath={PinkClouds}
                         title="newDND"
                         description="A Heroku deployed application that utilizes DataBase management and Javascript functionality along with NPM packages to create a Dungeon Crawler style game"
                         GitHubLink="https://github.com/rasvindra/newDND"
@@ -23,7 +23,7 @@ function Projects() {
                     </Col>
                     <Col md={4} className="project-card">
                         <ProjectCards
-                        imgPath={PinkClouds}
+                        // imgPath={PinkClouds}
                         title="US National Park Traveler's Dashboard"
                         description="Collaborative group project integrating data received from multiple server-side API requests."
                         GitHubLink="https://mwambaugh.github.io/collaborativeAPIstudy"
@@ -33,7 +33,7 @@ function Projects() {
                     </Col>
                     <Col md={4} className="project-card">
                         <ProjectCards
-                        imgPath={PinkClouds}
+                        // imgPath={PinkClouds}
                         title="Chew-on-it"
                         description="This is a social media application to discover new restaurants personally recommended by friends, family and peers. By utilizing Node.js, Express.js, Handlebars.js, MySQL and Sequelize, this is a dynamic full stack application that accepts and responds to user inputs to create reviews, add comments, and retrieve data and information."
                         GitHubLink="https://github.com/pem2k/Chew-on-it"
@@ -43,7 +43,7 @@ function Projects() {
                     </Col>
                     <Col md={4} className="project-card">
                         <ProjectCards
-                        imgPath={PinkClouds}
+                        // imgPath={PinkClouds}
                         title="Cute and Cuddly"
                         description="A simple page that links to third party API and provides pictures and a bit of information related to the searchable breed of various cats and dogs for user referance. Individual pages for dog search and cat search. and additioanl page with a game for users to choose which animal they think is cuter"
                         GitHubLink="https://github.com/GabrielWarner/cute-and-cuddly"
